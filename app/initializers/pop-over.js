ca9226f0e59221e47189c3e5567d852d2a213f7c
rogatty <igor@wikia-inc.com>
2018-01-11 15:51:58 +0100

    Remove unused and deprecated liquid-fire related code

--- app/initializers/pop-over.js
+++ app/initializers/pop-over.js
@@ -1,6 +1,5 @@
 import Ember from "ember";
 import Flow from "ember-pop-over/system/flow";
-import config from "../config/environment";
 import * as flows from "../flows";
 
 const get = Ember.get;
@@ -11,16 +10,6 @@ export var initialize = function (app) {
     if (flowName == 'default') { return; }
     let constraints = get(flows[flowName].call(Flow.create()), 'constraints');
     app.register(`pop-over-constraint:${flowName}`, constraints, { instantiate: false });
-  });
-
-  // Set flags for integrations with other addons
-  // At least during testing, `require.entries` is null / undefined, causing
-  // the initializer to fail. We need to guard against that.
-  let entries = require.entries || {};
-  let includedModules = Ember.A(keys(entries));
-  Ember.A(['liquid-fire']).forEach(function (moduleName) {
-    let includesIntegration = includedModules.contains(moduleName);
-    app.register(`pop-over-integrations:${moduleName}`, includesIntegration, { instantiate: false });
   });
 };
 
